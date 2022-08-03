701c7d2dde0dba83c11817b7f97c637d09efcbb6
Nikeex <dileshchouhan86@gmail.com>
2022-08-03 23:31:27 +0530

    lucky Integer

---
+++ 905.sort-array-by-parity.cpp
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=905 lang=cpp
+ *
+ * [905] Sort Array By Parity
+ */
+
+// @lc code=start
+#include<iostream>
+#include<vector>
+using namespace std; 
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& nums) {
+        list<int> a;
+        for(auto it: nums)
+        {
+            if(it % 2)
+                a.push_back(it);
+            else
+                a.push_front(it);
+        }
+        nums.clear();
+        for(auto it: a)
+            nums.push_back(it);
+        return nums;
+    }
+};
+// @lc code=end
+
